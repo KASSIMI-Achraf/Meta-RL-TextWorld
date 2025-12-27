17f186ac799fafc2f670fac5bba8980f1b57ab61
KASSIMI Achraf <achkass71@gmail.com>
2025-12-27 21:34:47 +0100

    Remove Docker configuration files

--- docker_run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Run a command inside the TextWorld Docker container
-REM Usage: docker_run.bat <command>
-REM Example: docker_run.bat python experiments/run_experiment.py --mode train
-
-docker exec -it my-textworld /bin/bash -c "cd /workspace && %*"
